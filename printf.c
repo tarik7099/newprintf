9b6724c9e3d8c05e0c2ecd85d5362f66b400b5cc
saadsgh123 <sdsghouri@gmail.com>
2023-10-14 23:14:00 +0100

    jdiedjeij

--- printf.c
+++ printf.c
@@ -16,25 +16,17 @@ int _putchar(char c)
 	return (write(1, &c, 1));
 }
 
-
-
-
-
-
-
-
-
-
 int _printf(const char *format, ...)
 {
-if (format == "\0")
+if (format == '\0')
 	return 0;
 int i = 0;
 int counter = 0;
 
 va_list args;
 va_start(args, format); 
-while(*format){
+while(*format)
+{
 	if(*format == '%')
 	{
 		format++;
@@ -43,12 +35,12 @@ while(*format){
 	{
 		print_string(va_arg(args, char*));
 		counter++;
-	}	
+	}
 		else if(*format == 'c')
-        {
-                print_char(va_arg(args, char*));
-		counter++;
-        }
+	{
+		print_char(va_arg(args, char*));
+		counter++;	
+	}
 		else if(*format ==  '%')
 	{
 		_putchar('%');
@@ -56,7 +48,7 @@ while(*format){
 
 	}
 		if (*format == '\0')
-                break;
+		break;
 		else if (*format == 'b')
 		{
 			decimalTobinary(va_arg(args,unsigned int));
@@ -71,41 +63,4 @@ format++;
 }
 va_end(args);
 return counter;
-}
-
-void print_string(char *str)
-{
-for (int i = 0;str[i] != '\0';i++ )
-	{
-		_putchar(str[i]);
-	}
-}
-void print_char(char *c)
-{
-	for (int i = 0; c[i]; c++) {
-        _putchar(c[i]);
-
-	}
-}
-void decimalTobinary(unsigned int d)
-{
-if (d == 0) {
-        _putchar('0');
-        return;
-    }
-int quotient[32];
-int i = 0;
-while(d > 0)
-{
-  quotient[i] = d % 2;
-  d = d / 2;
-
-i++;
-}
-for (int j = i - 1; j >= 0; j--) {
-       _putchar( quotient[j] + '0');
-}
-}
-int main()
-{
 }
