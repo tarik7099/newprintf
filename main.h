047ce1c000a8c81fff76e84a63a1c329fbe635ce
tarik <tarikkhadri>
2023-10-14 22:52:13 +0100

    printf

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+int _putchar(char c);
+void print_string(char *str);
+void print_char(char *c);
+int _printf(const char *format, ...);
+void decimalTobinary(unsigned int d);
+#endif
